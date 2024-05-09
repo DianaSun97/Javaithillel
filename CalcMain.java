38a4f98084247e6f9a69fedd079f8a0108b7b146
diana-erply <dsmirnova@erply.com>
2024-05-09 20:58:45 +0300

    Task home 14

--- CalcMain.java
+++ CalcMain.java
@@ -3,45 +3,47 @@ import java.util.Scanner;
 public class CalcMain {
     public static void main(String[] args) {
         System.out.println("You can order multiple drinks");
-        System.out.println("You can chose some drinks coffee, tea,  lemonade, moxito, water, coca_cola ");
+        System.out.println("You can chose some drinks coffee, tea,  lemonade, moxito, water, coca_cola");
         System.out.println("How many drinks you want chose");
         Scanner scan = new Scanner(System.in);
         Scanner scan2 = new Scanner(System.in);
         int n = scan.nextInt();
-        DrinksMachine s[] =new DrinksMachine[n];
+        DrinksMachine s[] = new DrinksMachine[n];
         int number[] = new int[n];
-        for (int i=0; i<n; i++){
-            while (true){
-                System.out.println("Your chose");
+        for (int i = 0; i < n; i++) {
+            while (true) {
+                System.out.println("What drink you want ?");
                 String choice = scan2.nextLine();
                 DrinksMachine dm = DrinksMachine.getFromString(choice);
-                if (dm == null){
+                if (dm == null) {
                     System.out.println("Error");
-                    }else{
-                    s[i]=dm;
+                } else {
+                    s[i] = dm;
                     break;
                 }
             }
             System.out.println("How match");
-            number[i]= scan.nextInt();
-        }
-    }
-    public static final int  price_coffee = 50;
-    public static final int price_tea = 25;
-    public static final int price_limonade = 15;
-    public static final int price_moxito = 45;
-    public static final int price_water = 10;
-    public static final int coca_cola = 18;
+            number[i] = scan.nextInt();
 
-    public static  int totalPrice (DrinksMachine s[], int number []){
+        }
+
+        System.out.println( "Count drinks "+getCount(number)+ " Price order "+totalPrice(s,number));
+
+    }
+
+    public static int totalPrice(DrinksMachine s[], int number[]) {
         int totalPrice = 0;
-        for(int i =0; i<s.length; i++){
-            switch (s[i]){
-                case coffee:
-                    totalPrice += price_coffee*number[i];
-                    break;
-            }
+        for (int i = 0; i < s.length; i++) {
+            totalPrice += s[i].getPrice() * number[i];
         }
         return totalPrice;
+    }
+
+    public static int getCount(int[] number) {
+        int count = 0;
+        for (int j : number) {
+            count += j;
+        }
+        return count;
     }
 }
--- DrinksMachine.java
+++ DrinksMachine.java
@@ -1,6 +1,13 @@
 public enum DrinksMachine {
-coffee, tea,  lemonade, moxito, water, coca_cola;
+coffee(50), tea(18),  lemonade(20), moxito(12), water(10), coca_cola(33);
+private int price;
+DrinksMachine(int  price){
+    this.price = price;
 
+}
+public int getPrice(){
+    return price;
+    }
     public static DrinksMachine getFromString(String s) {
           for (DrinksMachine i: DrinksMachine.values()){
               if (i.name().equals(s)){
